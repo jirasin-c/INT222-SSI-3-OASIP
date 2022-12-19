e90093fbdf895359f7840688f1eaff861d284342
Naruebet Phattarasanya <james@Naruebets-MacBook-Air.local>
2022-12-20 00:31:53 +0700

    Updated some fixs

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -307,7 +307,7 @@ onUpdated(() => {
               your teacher. Try out our features or click "Book Now" below to begin.</p>
             <div class="flex justify-center">
               <router-link :to="{name: 'Booking'}"
-                class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">BOOK
+                class="inline-flex text-white bg-sky-400 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">BOOK
                 NOW</router-link>
               <router-link :to="{name: 'EventList'}"
                 class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">View Booking</router-link>
